d339239876ffc4047f3d9515568644524a299dd3
Tommi Ronkainen <tommi.ronkainen@vincit.com>
2018-01-20 18:03:11 +0200

    Finalise transaction creation.

--- backend/bin/tx-test.js
+++ backend/bin/tx-test.js
@@ -5,7 +5,7 @@ const tx = require('../src/lib/tx');
 
 tx.add('dataplug', 1, '2018-01-20', [
   {number: 1910, amount: -20.50, description: 'TRE - HKI - TRE'},
-  {number: 7800, amount: 20.50, description: 'TRE - HKI - TRE'},
+  {number: 7800, amount: 20.50},
 ])
 .then((tx) => {
   console.log('OK', tx);
--- backend/src/lib/tx.js
+++ backend/src/lib/tx.js
@@ -1,6 +1,7 @@
 /**
  * A library for creating transactions.
  */
+const promiseSeq = require('promise-sequential');
 const moment = require('moment');
 const knex = require('./knex');
 const data = require('./data');
@@ -8,8 +9,8 @@ const data = require('./data');
 /**
  * Create new document into the database.
  *
- * @param {string} db
- * @param {number} periodId
+ * @param {string} db Name of the database.
+ * @param {number} periodId ID of the period.
  * @param {string} date A date in YYYY-MM-DD format.
  * @return {number} Document ID.
  */
@@ -32,18 +33,46 @@ function addDocument(db, periodId, date) {
 }
 
 /**
+ * Create new entry into the database.
+ *
+ * @param {string} db Name of the database.
+ * @param {number} accountId ID of the account.
+ * @param {number} documentId ID of the document, the entry belongs to.
+ * @param {number} debit If 1, then this is debit, otherwise credit.
+ * @param {number} amount
+ * @param {string} desc
+ * @param {number} row Order number of the entry 1..n.
+ * @param {number} flags
+ */
+function addEntry(db, accountId, documentId, debit, amount, desc, row, flags) {
+  return knex.db(db)('entry')
+    .insert({
+      document_id: documentId,
+      account_id: accountId,
+      debit: debit,
+      amount: amount,
+      description: desc,
+      row_number: row,
+      flags: flags,
+    });
+}
+
+/**
  * Insert transaction into the database.
  *
- * @param {string} db
- * @param {number} periodId
+ * @param {string} db Name of the database.
+ * @param {number} periodId ID of the period.
  * @param {string} date A date in YYYY-MM-DD format.
- * @param {array} txs
+ * @param {array} txs List of transactions.
+ * @return {array} A list on entry IDs created.
  *
  * The transaction is an array of entries like
  *   [
  *     {number: 1910, amount: -20.50, description: 'TRE - HKI - TRE'},
- *     {number: 7800, amount: 20.50, description: 'TRE - HKI - TRE'}
+ *     {number: 7800, amount: 20.50}
  *   ]
+ * Missing pieces are filled in as necessary. Account can be gien as a `number` or
+ * an `accountId`.
  */
 function add(db, periodId, date, txs) {
 
@@ -51,6 +80,10 @@ function add(db, periodId, date, txs) {
   let accountNumberToId = {};
   // Total amount in transaction.
   let total = 0;
+  // Counter for line number.
+  let line = 1;
+  // Last description.
+  let desc = null;
 
   // Helper to fill in missing information and collect sum for each entry.
   function prepare(tx) {
@@ -60,12 +93,25 @@ function add(db, periodId, date, txs) {
       }
       accountNumberToId[tx.number] = null;
     }
+
+    // Check is it debit or credit.
     tx.debit = 1;
     total += tx.amount;
     if (tx.amount < 0) {
       tx.amount = -tx.amount;
       tx.debit = 0;
     }
+
+    // Handle line number and description.
+    tx.row = line++;
+    if (tx.description) {
+      desc = tx.description;
+    } else {
+      tx.description = desc;
+    }
+
+    tx.flags = tx.flags || 0;
+
     return tx;
   }
 
@@ -76,21 +122,33 @@ function add(db, periodId, date, txs) {
     throw new Error('Invalid total ' + total + ' for TXs ' + JSON.stringify(txs));
   }
 
-  // Second helper to fill missing account IDs and making final checks.
+  // Second helper to fill in missing account IDs and making final checks.
   function fill(tx) {
     tx.accountId = tx.accountId || accountNumberToId[tx.number];
+    if (tx.amount === undefined) {
+      throw new Error('Missing `amount` in TX ' + JSON.stringify(tx));
+    }
+    if (tx.description === undefined) {
+      throw new Error('Missing `description` in TX ' + JSON.stringify(tx));
+    }
+    if (tx.accountId === undefined) {
+      throw new Error('Missing `accountId` in TX ' + JSON.stringify(tx));
+    }
     return tx;
   }
 
-  // Fill in account IDs, where missing.
+  // Fill in account IDs, where missing and do final checks.
   return Promise.all(Object.keys(accountNumberToId).map((number) => data.getAccountId(db, number)))
     .then((mapping) => {
       mapping.forEach((map) => accountNumberToId[map.number] = map.id);
       txs = txs.map((tx) => fill(tx));
 
-      return txs;
+      return addDocument(db, periodId, date);
     })
-  //return addDocument(db, periodId, date);
+    .then((documentId) => {
+      const creators = txs.map((tx) => () => addEntry(db, tx.accountId, documentId, tx.debit, tx.amount, tx.description, tx.row, tx.flags));
+      return promiseSeq(creators);
+    });
 }
 
 module.exports = {
