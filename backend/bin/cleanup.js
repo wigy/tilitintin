b4e7d7f7e4d60ec0895ecb58c622992d289376e7
Tommi Ronkainen <tommi.ronkainen@vincit.com>
2018-02-05 19:01:41 +0200

    Tool to remove documents not having any entries.

---
+++ backend/bin/cleanup.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+/**
+ * This script removes documents without any entries.
+ */
+const promiseSeq = require('promise-sequential');
+const knex = require('../src/lib/knex');
+const cli = require('../src/lib/cli');
+
+cli.arg('db', knex.dbs());
+
+knex.db(cli.db)
+  .count('entry.id AS entries')
+  .select('document.id', 'document.number')
+  .from('document')
+  .where('document.number', '>', 0)
+  .leftJoin('entry', 'entry.document_id', 'document.id')
+  .groupBy('document.id')
+  .having('entries', '=', 0)
+  .then((data) => {
+    if (!data.length) {
+      console.log('All good.');
+      return [];
+    }
+    console.log('Dropping documents', data.map((d) => d.number).join(', '));
+    return data.map((d) => () => knex.db(cli.db)('document').where({id: d.id}).delete());
+  })
+  .then((txs) => promiseSeq(txs));
