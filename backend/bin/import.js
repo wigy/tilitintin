12a3187f4aa30a95df28c035f3d0cfd05472a8fe
Tommi Ronkainen <tommi.ronkainen@vincit.com>
2018-02-08 17:58:21 +0200

    Add support for options. Make single import file.

---
+++ backend/bin/import.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+const cli = require('../src/lib/cli');
+const knex = require('../src/lib/knex');
+
+cli.opt('debug', false, 'To turn dry-run on.');
+cli.opt('service', null, 'Name of the service tag if any.');
+cli.opt('fund', null, 'Additional fund tag if any.');
+cli.opt('bank', 1910, 'Number of an account for banking.');
+cli.opt('euro', 1960, 'Number of an account for storing euros in service.');
+cli.opt('crypto', 1549, 'Number of an account for storing crypto currencies.');
+cli.opt('eth', null, 'Number of an account for storing ETH.');
+cli.opt('btc', null, 'Number of an account for storing BTC.');
+cli.opt('fees', 9690, 'Number of an account for trading fees.');
+cli.opt('rounding', 8570, 'Number of an account for rounding errors.');
+cli.opt('losses', 9750, 'Number of an account for recording losses.');
+cli.opt('profits', 3490, 'Number of an account for recoring profit.');
+cli.arg_('format', ['kraken', 'coinmotion']);
+cli.arg_('db', knex.dbs());
+cli.arg('csv-file', 'transaction log from Kraken as CSV file');
+
+const importer = require('../src/lib/import/' + cli.format);
+importer.configure({
+  service: cli.options.service,
+  fund: cli.options.fund,
+  accounts: {
+    bank: cli.options.bank,
+    euro: cli.options.euro,
+    crypto: cli.options.crypto,
+    eth: cli.options.eth,
+    btc: cli.options.btc,
+    fees: cli.options.fees,
+    rounding: cli.options.rounding,
+    losses: cli.options.losses,
+    profits: cli.options.profits
+  }
+});
+
+importer.import(cli.db, cli['csv-file'], cli.options.debug)
+  .then((num) => console.log('Created', num, 'entries.'))
+  .catch((err) => console.error(err));
--- backend/src/lib/cli.js
+++ backend/src/lib/cli.js
@@ -6,53 +6,93 @@ const clone = require('clone');
 class Cli {
 
   constructor() {
+    // Current list of unparsed args.
+    this.__remaining = null;
     // Current list of accepted args.
     this.__args = [];
     // Initial part of usage instructions.
     this.__usage = 'Usage: ' + process.argv[1];
-    // Current list of arg descriptions.
+    // Current list of argument or option descriptions.
     this.__desc = [];
+    // Currently defined options.
+    this.__options = {};
+    // Current option values.
+    this.options = {};
   }
 
   /**
-   * Return number of arguments.
+   * Define an option for the CLI.
+   * @param {String} name
+   * @param {any} initial
+   * @param {String} desc
    */
-  args() {
-    return process.argv.length - 2;
+  opt(name, initial, desc) {
+    this.__options[name] = {default: initial, description: desc};
+    this.options[name] = initial;
+    this.__desc.push('     --' + name + '\t' + desc);
+  }
+
+  /**
+   * Extract the options from the command line arguments.
+   */
+  parseOptions() {
+    while (this.__remaining[2] && this.__remaining[2].substr(0, 2) === '--') {
+      const opt = this.__remaining[2].substr(2, this.__remaining[2].length);
+      const match = /^(\w+)=(.*)/.exec(opt);
+      if (this.__options[opt] && !match) {
+        this.options[opt] = true;
+      } else {
+        if (!this.__options[match[1]]) {
+          console.error('Invalid option', opt);
+          process.exit(4);
+        }
+        this.options[match[1]] = match[2];
+      }
+      this.__remaining.splice(2, 1);
+    }
   }
 
   /**
    * Append a new arg and check if it has been given in commandline already.
    * @param {string} name Name of the arg.
-   * @param {array|string} options A list of values or value description.
+   * @param {array|string} values A list of values or value description.
    * @param {function|parseFloat|parseInt} check Checker function for validity.
    */
-  arg(name, options, check) {
+  arg(name, values, check) {
+
+    if (this.__remaining === null) {
+      this.__remaining = clone(process.argv);
+      this.parseOptions();
+    }
+
+    if (this.__desc.length) {
+      this.__desc.push('');
+    }
 
     const arg = '<' + name + '>';
     this.__usage += ' ' + arg;
 
-    let optionList = [];
+    let valueList = [];
     let promise = null;
 
-    while (typeof(options) !== 'string') {
-      if (options instanceof Array) {
-        optionList = clone(options).map((item) => "" + item);
-        options = options.length ? '`' + options.join('`, `') + '`' : 'nothing available';
-      } else if (options instanceof Function) {
-        options = options(this);
+    while (typeof(values) !== 'string') {
+      if (values instanceof Array) {
+        valueList = clone(values).map((item) => "" + item);
+        values = values.length ? '`' + values.join('`, `') + '`' : 'nothing available';
+      } else if (values instanceof Function) {
+        values = values(this);
       }
-      if (options === undefined) {
-        console.error('Failed to parse options for', arg);
+      if (values === undefined) {
+        console.error('Failed to parse values for', arg);
         process.exit(3);
       }
     }
 
-    options = '     ' + arg + '  is  ' + options;
-    this.__desc.push(options);
+    values = '     ' + arg + '\t' + values;
+    this.__desc.push(values);
 
     let n = this.__args.length + 2;
-    if (process.argv.length <= n) {
+    if (this.__remaining.length <= n) {
       if (this.__no_exit) {
         return;
       }
@@ -64,12 +104,12 @@ class Cli {
       process.exit(1);
     }
 
-    let value = process.argv[n];
+    let value = this.__remaining[n];
     let transform = (x) => x;
 
     if (!check) {
-      if (optionList.length) {
-        check = (value) => optionList.includes(value);
+      if (valueList.length) {
+        check = (value) => valueList.includes(value);
       } else {
         check = (value) => true;
       }
