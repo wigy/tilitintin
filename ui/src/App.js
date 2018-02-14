e8b440deb6cd9f121c32130b6903be275fe076a1
Tommi Ronkainen <tommi.ronkainen@vincit.com>
2018-02-14 18:36:21 +0200

    Create new component for menu.

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import './App.css';
-import Periods from './Components/Periods';
 import Balances from './Components/Balances';
 import AccountTransactions from './Components/AccountTransactions';
 import Accounts from './Components/Accounts';
 import Reports from './Components/Reports';
-import DbLinks from './Components/DbLinks';
 import Menu from './Components/Menu';
 import ToolPanel from './Components/ToolPanel';
 import Login from './Components/Login';
@@ -15,14 +13,11 @@ import Logout from './Components/Logout';
 export default (class App extends Component {
 
   render() {
-    // TODO: Top panel could be separate coponent.
     return (
       <div className="App">
         <div className="TopPanel Panel">
           <div style={{float: 'right'}} hreg="#"><Logout/></div>
-          <DbLinks />
-          <Route path="/:db" component={Periods}/>
-          <Route path="/:db" component={Menu}/>
+          <Route path="/" component={Menu}/>
         </div>
         <div className="SidePanel Panel">
           <div className="Frame">
--- ui/src/Components/Menu.js
+++ ui/src/Components/Menu.js
@@ -1,5 +1,56 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { inject, observer } from 'mobx-react';
+
+export default inject('store')(observer(class Menu extends Component {
+  render() {
+    if (!this.props.store.token) {
+      return '';
+    }
+    // TODO: Rename as Menu and get rit of old Menu.
+//    Databases: {this.props.store.dbs.map(db => (<Link key={db} to={'/' + db}>{db}</Link>))}
+    return (
+      <div className="Menu">
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <a class="navbar-brand" href="#">Tilitin</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Dropdown
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li>
+        </ul>
+      </div>
+      </nav>
+      </div>
+    );
+  }
+}));
+
+/*
+OLD Menu
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Menu extends Component {
   render() {
@@ -13,3 +64,8 @@ class Menu extends Component {
 }
 
 export default Menu;
+
+OLD Period list
+<Route path="/:db" component={Periods}/>
+
+*/
