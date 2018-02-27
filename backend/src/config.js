3b03c467fde5099d791584335e8c8158f306909e
Tommi Ronkainen <tommi.ronkainen@vincit.com>
2018-02-27 21:38:53 +0200

    Change default.

--- backend/src/config.js
+++ backend/src/config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   PORT: process.env.PORT || 3001,
-  BASEURL: process.env.BASEURL || '',
+  BASEURL: process.env.BASEURL || 'http://localhost:' + (process.env.PORT || '3001'),
   DBPATH: process.env.DBPATH || path.resolve(process.cwd(), 'databases'),
   SECRET: process.env.SECRET || 'oGBtRQBWIVUjnqw0nbmvbJ7zsd&67rtyr/r5fyft62',
   AUTHENTICATION: false,
