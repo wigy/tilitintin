2f09117abd1b355196db0e0344603245d3ebabb8
Tommi Ronkainen <tommi.ronkainen@vincit.com>
2018-04-15 20:22:50 +0300

    Option --avg.

--- backend/bin/import-ng.js
+++ backend/bin/import-ng.js
@@ -7,6 +7,7 @@ cli.opt('debug', null, 'To turn dry-run on and display entries.');
 cli.opt('no-profit', null, 'Turn off profit and losses calculations (to be calculated later).');
 cli.opt('zero-moves', null, 'Do not add to the stock commodities moved in.');
 cli.opt('force', null, 'Import even if the entries are found already.');
+cli.opt('avg', null, 'Set explicit averages `SERVICE1:ETH=123,SERVICE2:ETH=122`.');
 cli.arg_('db', knex.dbs());
 cli.args('csv-files', 'transaction log as CSV file(s)');
 
@@ -22,8 +23,19 @@ config.set({
   }
 });
 
+let avg={};
+
+if (cli.options.avg) {
+  cli.options.avg.split(',').forEach((str) => {
+    const eq = str.split('=');
+    avg = avg || {};
+    avg[eq[0]] = parseFloat(eq[1]);
+  })
+}
+
 async function main() {
   fyffe.setDb('tilitintin', knex.db(cli.db))
+  fyffe.setAverages(avg);
   await fyffe.import(cli['csv-files'], {dbName: 'tilitintin'});
   await fyffe.export('tilitintin', {dbName: 'tilitintin'});
 }
