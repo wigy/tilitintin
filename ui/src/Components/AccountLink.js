a4e85a9d9835c0945f70f084d99b55a996c1042b
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2019-01-05 18:39:33 +0200

    Define prop types.

--- ui/src/Components/AccountLink.js
+++ ui/src/Components/AccountLink.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 const AccountLink = (props) => {
@@ -6,6 +7,11 @@ const AccountLink = (props) => {
   return (<Link to={dst}>
     {props.account.number} {props.account.name}
   </Link>);
+};
+
+AccountLink.propTypes = {
+  account: PropTypes.object,
+  period: PropTypes.number
 };
 
 export default AccountLink;
