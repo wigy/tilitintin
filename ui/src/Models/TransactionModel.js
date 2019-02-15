86415887d9fd913604f34c7925f2a8a45f09d51d
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2019-02-15 18:22:44 +0200

    Implement Tab-key.

--- ui/src/Models/TransactionModel.js
+++ ui/src/Models/TransactionModel.js
@@ -46,6 +46,46 @@ class TransactionModel extends NavigationTargetModel {
   }
 
   /**
+   * Walk through columns and rows and add an entry, if on the last column of the last row.
+   * @param {Cursor} cursor
+   */
+  keyTab(cursor) {
+    if (cursor.row === null) {
+      cursor.setCell(0, 0);
+      // Since we enter on second column when coming from null-row, redo it.
+      return cursor.setCell(0, 0);
+    } else {
+      const model = cursor.getModel();
+      const [columns, rows] = model.geometry();
+      if (cursor.column < columns - 1) {
+        return cursor.setCell(cursor.column + 1, cursor.row);
+      } else if (cursor.row < rows - 1) {
+        return cursor.setCell(0, cursor.row + 1);
+      } else {
+        this.keyInsert(cursor);
+      }
+    }
+  }
+
+  /**
+   * Walk through columns and rows in reverse order.
+   * @param {Cursor} cursor
+   */
+  keyShiftTab(cursor) {
+    if (cursor.row !== null) {
+      const model = cursor.getModel();
+      const [columns] = model.geometry();
+      if (cursor.column > 0) {
+        return cursor.setCell(cursor.column - 1, cursor.row);
+      } else if (cursor.row > 0) {
+        return cursor.setCell(columns - 1, cursor.row - 1);
+      } else {
+        return cursor.setCell(null, null);
+      }
+    }
+  }
+
+  /**
    * Mark either document or entry for deletion.
    * @param {Cursor} cursor
    */
