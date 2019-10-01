45653fdd67c51626f583e04359af9b7c6aba5994
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2019-10-01 19:58:19 +0300

    Tool to create txs.

---
+++ backend/bin/tx.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+const knex = require('../src/lib/knex');
+const { config, util: {cli} } = require('libfyffe');
+const USER = process.env.FYFFE_USER || 'user';
+const tilitintinTx = require('libfyffe').data.tilitintin.tx;
+
+knex.setUser(USER);
+
+cli.arg_('db', knex.dbs(USER));
+cli.arg_('operation', ['add']);
+cli.arg('text', 'new-line separated string of entries as `2019-01-01 1234 -12.34 Description here\\n1234 12.34 Description here`');
+
+config.loadIni();
+const db = knex.db(cli.db);
+
+async function main() {
+  const text = cli.text.trim().replace(/\\n/g, '\n'); // Handle also literal \n as new line.
+  const entries = [];
+  let date;
+  for (const line of text.split('\n')) {
+    const [, date_, account, amount, description] = /^\s*(?:(\d\d\d\d-\d\d-\d\d)\s+)?(\d+)\s+(-?[0-9.]+)\s+(.*?)\s*$/.exec(line);
+    entries.push({
+      number: account,
+      amount: parseFloat(amount),
+      description
+    });
+    date = date || date_;
+  }
+  await tilitintinTx.add(db, date, null, entries);
+}
+
+main()
+  .then(() => process.exit())
+  .catch((err) => { console.error(err); process.exit(); });
