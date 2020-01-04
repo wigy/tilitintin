a012b7052ae875c956ee8c7e3e70dd538b344b8d
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2020-01-04 21:04:37 +0200

    Try cors.

--- ui/src/Stores/Store.js
+++ ui/src/Stores/Store.js
@@ -138,6 +138,7 @@ class Store {
       delete options.headers['Content-Type'];
       options.body = new FormData();
       options.body.set('file', file);
+      options.mode = 'no-cors';
     }
 
     debug('  Request:', method, config.API_URL + path, data || '');
