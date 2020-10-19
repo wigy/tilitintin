8c011d5d30f84be9ac3fedd38c8a3c264341006a
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2020-10-19 19:56:05 +0300

    Refactor balabce table.

--- ui/src/Components/BalanceTable.js
+++ ui/src/Components/BalanceTable.js
@@ -1,8 +1,41 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import BalanceLine from './BalanceLine';
-import { observer } from 'mobx-react';
+import { observer, inject } from 'mobx-react';
 import BalanceModel from '../Models/BalanceModel';
+import { action } from 'mobx';
+import { Link } from 'react-router-dom';
+import Cursor from '../Stores/Cursor';
+import Money from './Money';
+
+@inject('cursor')
+@observer
+class BalanceLine extends Component {
+
+  // Move to the clicked balance line.
+  @action.bound
+  onClick() {
+    this.props.cursor.setComponent('Balances.balances');
+    this.props.cursor.setIndex(this.props.index, { noScroll: true });
+  }
+
+  render() {
+    const { balance } = this.props;
+    const dst = balance.getUrl();
+    return (
+      <tr id={balance.getId()} className={balance.getClasses()}>
+        <td className="number"><Link onClick={() => this.onClick()} to={dst}>{balance.account && balance.account.number}</Link></td>
+        <td className="name"><Link onClick={() => this.onClick()} to={dst}>{balance.account && balance.account.name}</Link></td>
+        <td className="balance"><Link onClick={() => this.onClick()} to={dst}><Money cents={balance.total} currency="EUR"/></Link></td>
+      </tr>
+    );
+  }
+}
+
+BalanceLine.propTypes = {
+  balance: PropTypes.instanceOf(BalanceModel),
+  cursor: PropTypes.instanceOf(Cursor),
+  index: PropTypes.number
+};
 
 @observer
 class BalanceTable extends Component {
