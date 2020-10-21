33bdf048734f4df687a2ad245b4deaab44945f21
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2020-10-21 18:46:10 +0300

    Implement icon button.

--- ui/src/Components/IconButton.js
+++ ui/src/Components/IconButton.js
@@ -1,26 +1,18 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { withTranslation } from 'react-i18next';
-import './IconButton.css';
+import { Icon, IconButton } from '@material-ui/core';
 
 @withTranslation('translations')
 class ToolPanel extends Component {
 
   render() {
     const { t } = this.props;
-    let className = 'IconButton';
-    if ('toggle' in this.props && !this.props.toggle) {
-      className += ' off';
-    }
-    if ('disabled' in this.props && this.props.disabled) {
-      className += ' off';
-    }
+
     return (
-      <div className={className} title={t('icon-' + this.props.title)} onClick={() => !this.props.disabled && this.props.onClick()}>
-        <div className="fa-icon">
-          <i className={'fas fa-2x ' + this.props.icon}></i>
-        </div>
-      </div>
+      <IconButton color="secondary" title={t('icon-' + this.props.title)} disabled={this.props.disabled} onClick={() => this.props.onClick()}>
+        <Icon className={this.props.icon} style={{ fontSize: 30 }}/>
+      </IconButton>
     );
   }
 }
