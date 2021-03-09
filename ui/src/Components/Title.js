32eab01d05e42b22f9492117daf065bc64b8f931
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2021-03-09 16:38:57 +0200

    Get rid of Title.css.

--- ui/src/Components/Title.js
+++ ui/src/Components/Title.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
 import { Typography } from '@material-ui/core';
-import './Title.css';
 
 const Title = ({ children }) => {
-  return <div className="Title">
+  return <div style={{ paddingLeft: '2rem', marginBottom: '1rem', borderBottom: '1px solid rgba(0,0,0,0.1)' }}>
     <Typography className="text" variant="h5">{children}</Typography>
   </div>;
 };
