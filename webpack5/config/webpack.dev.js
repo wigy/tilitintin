e130732a4bbdf5540a123eff1344c6ea9b38cfbf
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2021-03-23 17:53:32 +0200

    Reorganize webpack config.

---
+++ webpack5/config/webpack.dev.js
@@ -0,0 +1,26 @@
+const paths = require("./paths");
+
+const webpack = require("webpack");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  // Set the mode to development or production
+  mode: "development",
+
+  // Control how source maps are generated
+  devtool: "inline-source-map",
+
+  // Spin up a server for quick development
+  devServer: {
+    historyApiFallback: true,
+    contentBase: paths.build,
+    compress: true,
+    hot: true,
+  },
+
+  plugins: [
+    // Only update what has changed on hot reload
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+});
