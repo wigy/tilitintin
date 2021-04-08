f7b3ae6302cd0928671a3c947f09dd061827290a
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2021-04-08 17:23:39 +0300

    Get version from package.json.

--- ui/src/Configuration.js
+++ ui/src/Configuration.js
@@ -1,10 +1,11 @@
-
+import pkg from '../package.json'
 //
 // AUTOMATICALLY GENERATED - DO NOT CHANGE ///
 //
 /* eslint quotes: off, comma-dangle: off */
 const config = {
-  VERSION: "2.0.1",
+  VERSION: pkg.version,
   API_URL: "http://localhost:3101",
 };
+
 export default config;
