3d66cf795abc87efb219f35c1c3bbe20062f2bbe
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2021-05-25 16:44:40 +0300

    Add Title class by default.

--- ui/src/Components/Title.jsx
+++ ui/src/Components/Title.jsx
@@ -3,7 +3,7 @@ import { PropTypes } from 'prop-types'
 import { Typography } from '@material-ui/core'
 
 const Title = ({ children, className }) => {
-  return <div className={className || ''} style={{ paddingLeft: '2rem', marginBottom: '1rem', borderBottom: '1px solid rgba(0,0,0,0.1)' }}>
+  return <div className={className || 'Title'} style={{ paddingLeft: '2rem', marginBottom: '1rem', borderBottom: '1px solid rgba(0,0,0,0.1)' }}>
     <Typography className="text" variant="h5">{children}</Typography>
   </div>
 }
