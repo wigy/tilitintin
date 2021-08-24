e9225b619520892432b3faa41572b7e0f5a5b369
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2021-08-24 19:10:38 +0300

    Back to creatrMuiTheme.

--- ui/src/theme.jsx
+++ ui/src/theme.jsx
@@ -1,7 +1,7 @@
-import { createTheme } from '@material-ui/core'
+import { createMuiTheme } from '@material-ui/core'
 import { green, orange } from '@material-ui/core/colors'
 
-const theme = createTheme({
+const theme = createMuiTheme({
   palette: {
     primary: {
       main: green[900],
