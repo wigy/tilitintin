143c2b00805e5a5fe284582873953a4c06a96c16
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2021-09-15 19:10:14 +0300

    Detect OS.

--- ui/src/Configuration.jsx
+++ ui/src/Configuration.jsx
@@ -1,9 +1,22 @@
 import pkg from '../package.json'
+import { detect } from 'detect-browser'
+
+const browser = detect()
 
 const Configuration = {
+  BROWSER: browser.name,
+  OS: browser.os,
   VERSION: pkg.version,
+  COMMAND_KEY: browser.os === 'iOS' ? '⌘' : 'Alt',
   // eslint-disable-next-line
   UI_API_URL: UI_API_URL
+}
+
+if (!Configuration.UI_API_URL) {
+  const siteUrl = new URL(document.location)
+  const parts = siteUrl.hostname.split('.')
+  parts[0] += '-api'
+  Configuration.UI_API_URL = `${siteUrl.protocol}://${parts.join('.')}${siteUrl.port ? ':' + siteUrl.port : ''}`
 }
 
 export default Configuration
