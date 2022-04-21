ec29dcf483a01492e2d352150b80365b2ba96723
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2022-04-21 19:05:57 +0300

    Simple api doc server.

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,10 +3,16 @@ const cors = require('cors')
 const config = require('./config')
 const app = express()
 const dump = require('neat-dump')
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('../../api/openapi.json');
+
+console.log(swaggerDocument);
 
 app.use(dump.middleware())
 app.use(cors())
 app.options('*', cors())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use('/', require('./routes/index'))
 
 app.listen(config.PORT, function () {
