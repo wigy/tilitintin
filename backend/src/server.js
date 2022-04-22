665c895173ef05c3a19c9b28d7a08c995dd3e587
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2022-04-22 15:33:08 +0300

    Change the api doc generation tool to neater version.

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,14 +3,11 @@ const cors = require('cors')
 const config = require('./config')
 const app = express()
 const dump = require('neat-dump')
-const swaggerUi = require('swagger-ui-express')
-
-const swaggerDocument = require('../api/openapi.json')
 
 app.use(dump.middleware())
 app.use(cors())
 app.options('*', cors())
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use(express.static('docs'))
 
 app.use('/', require('./routes/index'))
 
