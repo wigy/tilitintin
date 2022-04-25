0b894c5d918a0fe88f9f3fc049827a2aa365b780
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2022-04-25 20:05:59 +0300

    Rebuild.

--- backend/api/code/api.ts
+++ backend/api/code/api.ts
@@ -17,6 +17,13 @@ export interface paths {
   "/db/upload": {
     post: operations["uploadDatabase"];
   };
+  "/db/account/{databaseName}": {
+    get: operations["getAccounts"];
+  };
+  "/db/account/{databaseName}/{id}": {
+    get: operations["getAccount"];
+    patch: operations["updateAccount"];
+  };
 }
 
 export interface components {}
@@ -88,7 +95,7 @@ export interface operations {
              * @description Globally unique name for the database.
              * @example mydb2
              */
-            name: string;
+            name?: string;
           }[];
         };
       };
@@ -126,22 +133,17 @@ export interface operations {
     };
     requestBody: {
       content: {
-        "application/json": {
+        "application/json": paths["/db"]["get"]["responses"]["200"]["content"]["application/json"]["schema"]["items"] & {
           /**
            * @description A code of the supported scheme available.
            * @example SchemaName
            */
           scheme: string;
           /**
-           * @description Globally unique name for the database.
-           * @example mydb2
-           */
-          databaseName: string;
-          /**
            * @description Name of the company,
            * @example Company Inc.
            */
-          companyName?: string;
+          companyName: string;
           /**
            * @description Official registration code of the company.
            * @example 1234567-US
@@ -159,6 +161,8 @@ export interface operations {
            * @enum {string}
            */
           currency?: "EUR" | "ZAR" | "USD";
+        } & {
+          databaseName: unknown;
         };
       };
     };
@@ -175,6 +179,18 @@ export interface operations {
       204: never;
       400: paths["/db"]["post"]["responses"]["400"];
       403: paths["/db"]["get"]["responses"]["403"];
+      /** Required target not found. */
+      404: {
+        content: {
+          "application/json": {
+            /**
+             * @description Error message.
+             * @example Not found.
+             */
+            message: string;
+          };
+        };
+      };
     };
   };
   uploadDatabase: {
@@ -205,6 +221,134 @@ export interface operations {
            */
           file?: string;
         };
+      };
+    };
+  };
+  getAccounts: {
+    parameters: {
+      path: {
+        /** Name of the database. */
+        databaseName: string;
+      };
+    };
+    responses: {
+      /** Get the list of all accounts in the database. */
+      200: {
+        content: {
+          "application/json": {
+            /** @description The ID of the account. */
+            id?: number;
+            /**
+             * @description Numeric code of the account as a string.
+             * @example 3000
+             */
+            number?: string;
+            /**
+             * @description Name of the account.
+             * @example Savings Bank Account
+             */
+            name?: string;
+            /**
+             * @description Language used in the database.
+             * @example ASSET
+             * @enum {string}
+             */
+            type?:
+              | "ASSET"
+              | "LIABILITY"
+              | "EQUITY"
+              | "REVENUE"
+              | "EXPENSE"
+              | "PROFIT_PREV"
+              | "PROFIT";
+            /**
+             * @description Language of the account name translation.
+             * @example en
+             * @enum {string}
+             */
+            language?: "fi" | "en";
+            /**
+             * @description Currency used in the account.
+             * @example USD
+             * @enum {string}
+             */
+            currency?: "EUR" | "ZAR" | "USD";
+            /**
+             * @description If set, this account can be filtered as favourite account.
+             * @example true
+             */
+            favourite?: boolean;
+            /** @description The tax type code. */
+            tax?: string;
+          }[];
+        };
+      };
+      403: paths["/db"]["get"]["responses"]["403"];
+    };
+  };
+  getAccount: {
+    parameters: {
+      path: {
+        /** Name of the database to delete. */
+        databaseName: string;
+        /** The ID of the account. */
+        id: number;
+      };
+    };
+    responses: {
+      /** Get the details of the account. */
+      200: {
+        content: {
+          "application/json": paths["/db/account/%7BdatabaseName%7D"]["get"]["responses"]["200"]["content"]["application/json"]["schema"]["items"] & {
+            /** @description List of periods that account has transactions. */
+            periods?: ({
+              /** @description The ID of the period. */
+              id?: number;
+              /**
+               * Format: date
+               * @description The first date included in the period.
+               * @example 2001-01-01
+               */
+              start_date?: string;
+              /**
+               * Format: date
+               * @description The last date included in the period.
+               * @example 2001-12-31
+               */
+              end_date?: string;
+              /** @description If set, the period transactions cannot be modified anymore. */
+              locked?: boolean;
+            } & {
+              /**
+               * @description The number of transactions for the account in this period.
+               * @example 31
+               */
+              entries?: number;
+            })[];
+          };
+        };
+      };
+      403: paths["/db"]["get"]["responses"]["403"];
+      404: paths["/db/%7BdatabaseName%7D"]["delete"]["responses"]["404"];
+    };
+  };
+  updateAccount: {
+    parameters: {
+      path: {
+        /** Name of the database to delete. */
+        databaseName: string;
+        /** The ID of the account. */
+        id: number;
+      };
+    };
+    responses: {
+      /** Account updated successfully. */
+      204: never;
+      403: paths["/db"]["get"]["responses"]["403"];
+    };
+    requestBody: {
+      content: {
+        "application/json": paths["/db/account/%7BdatabaseName%7D"]["get"]["responses"]["200"]["content"]["application/json"]["schema"]["items"];
       };
     };
   };
